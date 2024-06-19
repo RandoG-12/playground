8ecbfdc5f2a4652390038c27e4c464bb08a3b6b6
RandoG <111612660+RandoG-12@users.noreply.github.com>
2024-06-19 15:14:04 -0400

    add complete without session

--- week-3/lecture3/tasks/views.py
+++ week-3/lecture3/tasks/views.py
@@ -1,22 +1,35 @@
 from django import forms
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # "global" vars to the app?
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 # create a form with one field with the name "task"
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
     
 
 # Create your views here.
-def index(request): 
+def index(request: HttpRequest): 
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
 
-def add(request):
+def add(request: HttpRequest):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():   #server side validation
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+            
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
