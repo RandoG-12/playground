3d80e25693e1e653253c88f0db708301f535108c
RandoG <111612660+RandoG-12@users.noreply.github.com>
2024-06-19 13:50:21 -0400

    tasks app with looping

---
+++ week-3/lecture3/tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # /tasks
+    path("", views.index, name="index")
+]
---
+++ week-3/lecture3/tasks/views.py
@@ -0,0 +1,11 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+# "global" vars to the app?
+tasks = ["foo", "bar", "baz"]
+
+# Create your views here.
+def index(request): 
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
