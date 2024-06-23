ec15ed3b8813e23703c99d061e455d4ac6b0bf48
RandoG <111612660+RandoG-12@users.noreply.github.com>
2024-06-23 18:07:30 -0400

    airport model

--- week-4/airline/flights/models.py
+++ week-4/airline/flights/models.py
@@ -1,3 +1,18 @@
+from turtle import mode
 from django.db import models
 
-# Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.code}\t{self.city}"
+
+class Flight(models.Model):
+    # origin = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE) #YUCK!
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.origin}\t{self.destination}\t{self.duration}"
